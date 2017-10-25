7c040d5499eb428baa51d71e5f91098972510ff3
Kaloyan Raev <kaloyan@raev.name>
2017-10-25 19:15:01 +0200

    Initial app skeleton - just lists the buckets

---
+++ src/main/java/io/goobox/sync/storj/App.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2017 Kaloyan Raev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.goobox.sync.storj;
+
+import io.storj.libstorj.Bucket;
+import io.storj.libstorj.GetBucketsCallback;
+import io.storj.libstorj.KeysNotFoundException;
+import io.storj.libstorj.Storj;
+
+public class App {
+    public static void main(String[] args) {
+        Storj.appDir = new java.io.File(System.getProperty("user.home"), ".storj").getAbsolutePath();
+        Storj storj = Storj.getInstance();
+        try {
+            storj.getBuckets(new GetBucketsCallback() {
+                @Override
+                public void onError(String message) {
+                    System.out.println(message);
+                }
+
+                @Override
+                public void onBucketsReceived(Bucket[] buckets) {
+                    String user = Storj.getInstance().getKeys("").getUser();
+                    System.out.println(buckets.length + " buckets in " + user);
+                    System.out.println("---------------------------------------------");
+                    for (Bucket bucket : buckets) {
+                        System.out.println(bucket.getId() + " " + bucket.getName());
+                    }
+                }
+            });
+        } catch (KeysNotFoundException e) {
+            System.out.println("No keys found. Have your imported your keys using libstorj? Make sure you don't specify a passcode.");
+        }
+    }
+}
