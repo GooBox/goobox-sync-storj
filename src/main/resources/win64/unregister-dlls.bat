1564fc07147de115a009fff1226ba62d4acd9032
Kaloyan Raev <kaloyan@raev.name>
2018-03-28 19:07:01 +0300

    Add Windows-specific resources to release artifact

---
+++ src/main/resources/win64/unregister-dlls.bat
@@ -0,0 +1,5 @@
+regsvr32 -u GooboxOverlay1OK_x64.dll
+regsvr32 -u GooboxOverlay2Syncing_x64.dll
+regsvr32 -u GooboxOverlay3Warning_x64.dll
+regsvr32 -u GooboxOverlay4Error_x64.dll
+regsvr32 -u GooboxContextMenus_x64.dll
